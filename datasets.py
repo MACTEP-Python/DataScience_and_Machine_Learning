bc838b56f0fd26bbf7e7e858aa3c8e504d4c23bb
Andrey Kabetskiy <57221680+MACTEP-Python@users.noreply.github.com>
2022-02-05 19:34:44 +0300

    Написал функцию, которая через requests забирает файлик с датасетом и сохраняет куда надо, потому что фун-я пандас read_csv иногда падает в ошибку, если указываешь http путь до датасета

---
+++ datasets.py
@@ -0,0 +1,23 @@
+def get_dataset(url: str = None, p: str = None):
+    """Функция получает на вход HTTP ссылку на файл с данными и сохраняет его в соответствующий файл.
+    Иногда функция пандас read_csv с переданным http выдет ошибку, данная функция это исправляет"""
+    import requests
+    file_name = url.split('/')[-1]
+    try:
+        resp = requests.get(url)
+        status_code = resp.status_code
+        if status_code == 200:
+            with open(f'{p}/{file_name}', 'w') as f:
+                f.write(resp.text)
+            print('Success!!!')
+            return f'{p}/{file_name}'
+        else:
+            print(f'Somthing wrong with url: Error {status_code} !!!')
+    except:
+        print(f'Error of downloading file...')
+
+
+if __name__ == "__main__":
+    url = 'https://stepik.org/media/attachments/course/4852/my_stat_1.csv'
+    p = 'Data'
+    print(get_dataset(url, p))
